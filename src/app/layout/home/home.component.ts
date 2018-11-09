27cb1c408974efd1280bb67f79d9fb6594f8f8e1
ikismail <ikismail7@gmail.com>
2018-11-09 21:55:33 +0530

    Updated Homepage

--- src/app/layout/home/home.component.ts
+++ src/app/layout/home/home.component.ts
@@ -2,6 +2,7 @@ import { User } from "./../../shared/models/User";
 import { particles } from "./../../shared/config/particle";
 import { Component, OnInit, ViewEncapsulation } from "@angular/core";
 declare var particlesJS: any;
+
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
@@ -17,7 +18,7 @@ export class HomeComponent implements OnInit {
       name: "Mohammed Ismail",
       position: "Software Engineer",
       bio: "Angular | Java | Web-service | Github",
-      avatar: "",
+      avatar: "https://avatars1.githubusercontent.com/u/17190913?s=460&v=4",
       email: "ikismail7@gmail.com",
       phoneNumber: "8124794942",
       link: {
--- src/app/layout/home/home.module.ts
+++ src/app/layout/home/home.module.ts
@@ -3,9 +3,10 @@ import { SharedModule } from "./../../shared/shared.module";
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { HomeComponent } from "./home.component";
+import { GithubCalendarComponent } from "./github-calendar/github-calendar.component";
 
 @NgModule({
   imports: [CommonModule, SharedModule, HomeRoutes],
-  declarations: [HomeComponent]
+  declarations: [HomeComponent, GithubCalendarComponent]
 })
 export class HomeModule {}
